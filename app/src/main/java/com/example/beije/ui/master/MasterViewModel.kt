e4e184b5cf78c7c753e5419f9c65cba42801e8c7
giovy10 <gv@spitalia.it>
2020-09-28 15:04:43 +0200

    Renamed master screen and detail screen

--- app/src/main/java/com/example/beije/ui/master/MasterViewModel.kt
+++ app/src/main/java/com/example/beije/ui/master/MasterViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.beije.ui.home
+package com.example.beije.ui.master
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class MasterViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
